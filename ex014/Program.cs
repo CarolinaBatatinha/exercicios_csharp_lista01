c091a625b082a7e7f428ce26c742e68752e08a6d
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-07-10 15:20:21 -0300

    Add files via upload

---
+++ ex014/Program.cs
@@ -0,0 +1,18 @@
+﻿//	Faça o cálculo da área de um retângulo.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe a altura do retângulo em centímetros (cm): ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Informe a base do retângulo em centímetros (cm): ");
+        double largura = Convert.ToDouble(Console.ReadLine());
+
+        double area = largura * altura;
+
+        Console.WriteLine($"A área do retângulo é de {area} cm².");
+    }
+}
